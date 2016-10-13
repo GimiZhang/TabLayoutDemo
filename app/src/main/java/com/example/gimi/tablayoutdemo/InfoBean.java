632a8687e75cb4f72c68aa88fe57331c8bdae165
luckyrobot@163.com <z121318>
2016-10-13 15:09:12 +0800

    [tablayout]

---
+++ app/src/main/java/com/example/gimi/tablayoutdemo/InfoBean.java
@@ -0,0 +1,14 @@
+package com.example.gimi.tablayoutdemo;
+
+/**
+ * Created by Administrator on 2016/10/13.
+ */
+
+public class InfoBean {
+    public String info;
+
+    public InfoBean(String info){
+        super();
+        this.info = info;
+    }
+}
--- app/src/main/java/com/example/gimi/tablayoutdemo/MyFragment.java
+++ app/src/main/java/com/example/gimi/tablayoutdemo/MyFragment.java
@@ -1,12 +1,19 @@
 package com.example.gimi.tablayoutdemo;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import com.example.gimi.tablayoutdemo.databinding.LayoutFragmentBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Administrator on 2016/5/5.
@@ -14,6 +21,8 @@ import android.widget.TextView;
 public class MyFragment extends Fragment {
 
     private TextView tv;
+    private LayoutFragmentBinding binding;
+    private List<InfoBean> list = new ArrayList<>();
 
     static MyFragment newInstance(String s){
         MyFragment myFragment = new MyFragment();
@@ -26,13 +35,16 @@ public class MyFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.layout_fragment,container,false);
+//        View view = inflater.inflate(R.layout.layout_fragment,container,false);
+        binding = DataBindingUtil.inflate(inflater, R.layout.layout_fragment,container,false);
+        for (int i = 'a'; i < 'z'; i++) {
+
+            list.add(new InfoBean(i+""));
+        }
         Bundle bundle = getArguments();
         String data = bundle.getString("DATA");
-        tv = (TextView) view.findViewById(R.id.id_fm_tv);
-        if(data != null){
-            tv.setText(data);
-        }
-        return view;
+        binding.idRecycle.setLayoutManager(new LinearLayoutManager(getActivity()));
+        binding.idRecycle.setAdapter(new FragmentAdapter(getActivity(),list));
+        return binding.getRoot();
     }
 }
